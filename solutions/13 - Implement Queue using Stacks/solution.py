6d433e5cd59400f0b692d71e74485674c35b4dfe
Francisco Sandi <fransandi@gmail.com>
2023-02-03 16:34:23 -0500

    13 - Implement Queue using Stacks

--- solutions/13 - Implement Queue using Stacks/solution.py
+++ solutions/13 - Implement Queue using Stacks/solution.py
@@ -6,5 +6,45 @@ LeetCode: https://leetcode.com/problems/implement-queue-using-stacks/
 """
 
 
-def solution(input):
-    pass
+class MyQueue:
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, el):
+        self.in_stack.append(el)
+
+    def pop(self):
+        self.peek()
+        return self.out_stack.pop()
+
+    def peek(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]
+
+    def empty(self):
+        return len(self.in_stack) + len(self.out_stack) == 0
+
+
+def solution(instructions):
+    queue = None
+
+    for i in range(len(instructions[0])):
+        instruction, val = instructions[0][i], instructions[1][i]
+
+        if instruction == "MyQueue":
+            queue = MyQueue()
+
+        if instruction == "push":
+            queue.push(val)
+
+        if instruction == "pop":
+            print(queue.pop())
+
+        if instruction == "peek":
+            print(queue.peek())
+
+        if instruction == "empty":
+            print(queue.empty())
