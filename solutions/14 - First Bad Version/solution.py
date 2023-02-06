01bb6800a85a419d005e6c6781307ddcb6efcb20
Francisco Sandi <fransandi@gmail.com>
2023-02-06 17:21:58 -0500

    21 - Diameter of Binary Tree

--- solutions/14 - First Bad Version/solution.py
+++ solutions/14 - First Bad Version/solution.py
@@ -5,31 +5,24 @@ Time: 20 min
 LeetCode: https://leetcode.com/problems/first-bad-version/
 """
 
-FIRST_BAD_VERSION = None
-
-
-def initialize_bad_version(bad):
-    global FIRST_BAD_VERSION
-    FIRST_BAD_VERSION = bad
-
-
-def isBadVersion(version: int) -> bool:
-    return version == FIRST_BAD_VERSION
-
 
 # Binary Search -> Time: O(log n), Space: O(1), where n is the number of versions n
 def solution(n, bad):
-    initialize_bad_version(bad)
+    first_bad_version = bad
+
+    def is_bad_version(version: int) -> bool:
+        nonlocal first_bad_version
+        return version == first_bad_version
+
+    def find_first_bad_version(lower, higher):
+        if lower == higher:
+            return lower
+
+        mid = (lower + higher) // 2
+
+        if is_bad_version(mid):
+            return find_first_bad_version(lower, mid)
+        else:
+            return find_first_bad_version(mid + 1, higher)
+
     return find_first_bad_version(0, n)
-
-
-def find_first_bad_version(lower, higher):
-    if lower == higher:
-        return lower
-
-    mid = (lower + higher) // 2
-
-    if isBadVersion(mid):
-        return find_first_bad_version(lower, mid)
-    else:
-        return find_first_bad_version(mid + 1, higher)
--- solutions/21 - Diameter of Binary Tree/solution.py
+++ solutions/21 - Diameter of Binary Tree/solution.py
@@ -5,6 +5,25 @@ Time: 30 min
 LeetCode: https://leetcode.com/problems/diameter-of-binary-tree/
 """
 
+from helpers.binary_tree import array_to_binary_tree
 
-def solution(input):
-    pass
+
+# Time: O(n), Space: O(n), where n is the number of nodes in the binary tree
+def solution(root):
+    root = array_to_binary_tree(root)
+    max_diameter = 0
+
+    def calculate_max_diameter(root):
+        if root is None:
+            return 0
+
+        left = calculate_max_diameter(root.left)
+        right = calculate_max_diameter(root.right)
+
+        nonlocal max_diameter
+        max_diameter = max(max_diameter, left + right)
+
+        return max(left, right) + 1
+
+    calculate_max_diameter(root)
+    return max_diameter
